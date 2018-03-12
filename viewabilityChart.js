d44f4b259a3a8925fcf95fafde8ab58739782673
Patrick Brady <patrick.brady@celtra.com>
2018-03-12 17:12:28 -0400

    update internal JS

--- viewabilityChart.js
+++ viewabilityChart.js
@@ -1,38 +1,38 @@
-var initViewabilityChart = function() {
-    var index = 0;
-    var ctx = document.getElementById("viewabilityChart").getContext('2d');
-    var viewabilityChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: [],
-            datasets: [{
-                label: '% in View',
-                data: [],
-                borderColor: 'rgba(0,0,0,1)',
-                borderWidth: 2,
-                cubicInterpolationMode: 'monotone'
+var index = 0;
+var ctx = document.getElementById("viewabilityChart").getContext('2d');
+var viewabilityChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: [],
+        datasets: [{
+            label: '% in View',
+            data: [],
+            borderColor: 'rgba(0,0,0,1)',
+            borderWidth: 2,
+            cubicInterpolationMode: 'monotone'
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
             }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero:true
-                    }
-                }]
-            }
         }
-    });
+    }
+});
 
-    var addRandomData = function(){
-        addData(Math.floor(Math.random()*100));
-    };
+var addRandomData = function() {
+    addData(Math.floor(Math.random()*100));
+};
 
-    var addData = function(data){
-        viewabilityChart.data.labels.push(index++ + "");
-        viewabilityChart.data.datasets[0].data.push(data);
-        viewabilityChart.update();
-    };
+var addData = function(data) {
+    viewabilityChart.data.labels.push(index++ + "");
+    viewabilityChart.data.datasets[0].data.push(data);
+    viewabilityChart.update();
+};
 
+var initViewabilityChart = function() {
     setInterval(addRandomData, 1000);
 };
