bf6fb49bf14dede6963c6fbbc9e0b7d2939b6abe
Patrick Brady <patrick.brady@celtra.com>
2018-03-12 16:56:05 -0400

    set up for MRAID environment

--- viewabilityChart.js
+++ viewabilityChart.js
@@ -1,36 +1,38 @@
-var index = 0;
-var ctx = document.getElementById("viewabilityChart").getContext('2d');
-var viewabilityChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: [],
-        datasets: [{
-            label: '% in View',
-            data: [],
-            borderColor: 'rgba(0,0,0,1)',
-            borderWidth: 2,
-            cubicInterpolationMode: 'monotone'
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
+var initViewabilityChart = function() {
+    var index = 0;
+    var ctx = document.getElementById("viewabilityChart").getContext('2d');
+    var viewabilityChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: [],
+            datasets: [{
+                label: '% in View',
+                data: [],
+                borderColor: 'rgba(0,0,0,1)',
+                borderWidth: 2,
+                cubicInterpolationMode: 'monotone'
             }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
         }
-    }
-});
+    });
 
-var addRandomData = function(){
-    addData(Math.floor(Math.random()*100));
+    var addRandomData = function(){
+        addData(Math.floor(Math.random()*100));
+    };
+
+    var addData = function(data){
+        viewabilityChart.data.labels.push(index++ + "");
+        viewabilityChart.data.datasets[0].data.push(data);
+        viewabilityChart.update();
+    };
+
+    setInterval(addRandomData, 1000);
 };
-
-var addData = function(data){
-    viewabilityChart.data.labels.push(index++ + "");
-    viewabilityChart.data.datasets[0].data.push(data);
-    viewabilityChart.update();
-}
-
-setInterval(addRandomData, 1000);
